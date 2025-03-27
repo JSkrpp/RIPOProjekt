85005fa05afff0fed4314a86d794dbe10c44708c
JSkrpp <132712962+JSkrpp@users.noreply.github.com>
2025-03-27 21:31:52 +0100

    Added interface window, web camera view

--- Interface.py
+++ Interface.py
@@ -13,17 +13,17 @@ class Interface:
         self.m.maxsize(1920,1080)
         self.m.bind('<Escape>', lambda e: self.m.quit())
 
-        self.label_widget = tk.Label(self.m)
-        self.label_widget.pack()
+        self.label_widget = tk.Label(self.m, width=854, height=480)
+        self.label_widget.place(x=0, y=10)
 
         self.cap = cv2.VideoCapture(0)
 
-        width, heigth = 1280, 720
+        width, heigth = 854,480
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, heigth)
 
         self.button1 = tk.Button(self.m, text="Open Camera", command =self.open_camera)
-        self.button1.pack()
+        self.button1.place(x=900, y=1000)
 
         self.m.mainloop()
 
