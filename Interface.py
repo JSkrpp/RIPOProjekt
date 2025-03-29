f8d9c1034c7532c514f55a3a5a0483615d38f294
JSkrpp <132712962+JSkrpp@users.noreply.github.com>
2025-03-29 11:23:32 +0100

    Implemented the display of the model picture and taking camera snaps.

--- Interface.py
+++ Interface.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from tkinter import filedialog, messagebox
 import cv2
 import cv2 as cv
 from PIL import Image, ImageTk
@@ -15,20 +15,33 @@ class Interface:
 
         self.label_widget = tk.Label(self.m, width=854, height=480)
         self.label_widget.place(x=0, y=10)
+        self.photo_widget = tk.Label(self.m, width=854, height=480)
+        self.photo_widget.place(x=1000, y=10)
+        self.model_widget = tk.Label(self.m, width=854, height=480)
+        self.model_widget.place(x=1000, y=530)
 
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv.VideoCapture(0)
+        self.camera_started = False
 
-        width, heigth = 854,480
+        width, heigth = 854, 480
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, heigth)
 
         self.button1 = tk.Button(self.m, text="Open Camera", command =self.open_camera)
-        self.button1.place(x=900, y=1000)
+        self.button1.place(x=350, y=500)
+
+        self.button2 = tk.Button(self.m, text="Take a picture", command=self.take_picture)
+        self.button2.place(x=450, y=500)
+
+        self.button3 = tk.Button(self.m, text="Choose a photo for the model", command = self.open_file_explorer)
+        self.button3.place(x=1400, y =1020)
 
         self.m.mainloop()
 
 
     def open_camera(self):
+        self.camera_started = True
+
         _, frame = self.cap.read()
 
         opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
@@ -42,3 +55,36 @@ class Interface:
         self.label_widget.configure(image=photo_image)
 
         self.label_widget.after(10, self.open_camera)
+
+    def open_file_explorer(self):
+        # Open file explorer to select an image file
+        file_path = tk.filedialog.askopenfilename(
+            title="Select an Image File",
+            filetypes=(("Image Files", "*.jpg;*.jpeg;*.png;*.bmp;*.tiff"), ("All Files", "*.*"))
+        )
+
+        if file_path:
+            # Load and display the selected image
+            selected_image = Image.open(file_path)
+            selected_image = selected_image.resize((854, 480))  # Resize image to fit the label widget
+            photo_image = ImageTk.PhotoImage(selected_image)
+
+            self.model_widget.photo_image = photo_image
+            self.model_widget.configure(image=photo_image)
+
+    def take_picture(self):
+        if not self.camera_started:
+            messagebox.showwarning("Warning", "Camera output has not been started yet!")
+            return
+
+        if self.cap and self.cap.isOpened():
+            _, frame = self.cap.read()
+            opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+            captured_image = Image.fromarray(opencv_image)
+
+            # Resize for the photo widget
+            captured_image_resized = captured_image.resize((854, 480))
+            photo_image = ImageTk.PhotoImage(image=captured_image_resized)
+
+            self.photo_widget.photo_image = photo_image
+            self.photo_widget.configure(image=photo_image)
